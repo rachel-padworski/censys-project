835a906ebc77a6274f9b3ddc8567be89c4ff58d7
Rachel Padworski <rachelpadworski@gmail.com>
2022-03-18 18:25:45 -0600

    adds geolocater class and css

--- src/geolocater.jsx
+++ src/geolocater.jsx
@@ -1,14 +1,3 @@
-// const app = require('express')();
-
-// app.get('/', (req, res) =>
-//     res.json({message: 'Docker is easy?'})
-// );
-
-// const port = process.env.PORT || 8080;
-
-// app.listen(port, () => console.log(`app listening to http://localhost:${port}`))
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
