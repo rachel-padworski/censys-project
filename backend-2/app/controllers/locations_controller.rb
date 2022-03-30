202455f15ffd026db98473e1037c1551e67b690e
Rachel Padworski <rachelpadworski@gmail.com>
2022-03-30 10:54:22 -0600

    updates backend directory

---
+++ backend-2/app/controllers/locations_controller.rb
@@ -0,0 +1,18 @@
+require 'maxmind/geoip2'
+
+class LocationsController < ApplicationController
+  
+
+  def index
+    
+  end
+
+  # GET /locations
+  def get_location
+    reader = MaxMind::GeoIP2::Reader.new('app/assets/GeoLite2-City.mmdb')
+    record = reader.city(params[:ip_address])
+    
+    render json: {location: record.location}
+  end
+ 
+end
